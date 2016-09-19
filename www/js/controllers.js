568ffa775f76008f04b4fda02f5de56a1d0ab93d
nampreetsarao <nampreetsarao@gmail.com>
2016-09-19 11:04:22 +0530

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,130 @@
+angular.module('app.controllers', [])
+  
+.controller('tilesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cartCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloudCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('accountCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('accountDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tagsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('moveMoneyCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('createVoucherCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('redeemVoucherCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('remindersCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('insightsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('smartToolsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('profileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,153 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.tiles', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/tiles.html',
+        controller: 'tilesCtrl'
+      }
+    }
+  })
+
+  .state('cart', {
+    url: '/page2',
+    templateUrl: 'templates/cart.html',
+    controller: 'cartCtrl'
+  })
+
+  .state('cloud', {
+    url: '/page3',
+    templateUrl: 'templates/cloud.html',
+    controller: 'cloudCtrl'
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('login', {
+    url: '/page4',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('menu.signup', {
+    url: '/page5',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/signup.html',
+        controller: 'signupCtrl'
+      }
+    }
+  })
+
+  .state('account', {
+    url: '/page7',
+    templateUrl: 'templates/account.html',
+    controller: 'accountCtrl'
+  })
+
+  .state('accountDetails', {
+    url: '/page8',
+    templateUrl: 'templates/accountDetails.html',
+    controller: 'accountDetailsCtrl'
+  })
+
+  .state('menu.tags', {
+    url: '/page9',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/tags.html',
+        controller: 'tagsCtrl'
+      }
+    }
+  })
+
+  .state('menu.moveMoney', {
+    url: '/page10',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/moveMoney.html',
+        controller: 'moveMoneyCtrl'
+      }
+    }
+  })
+
+  .state('menu.createVoucher', {
+    url: '/page11',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/createVoucher.html',
+        controller: 'createVoucherCtrl'
+      }
+    }
+  })
+
+  .state('menu.redeemVoucher', {
+    url: '/page12',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/redeemVoucher.html',
+        controller: 'redeemVoucherCtrl'
+      }
+    }
+  })
+
+  .state('menu.reminders', {
+    url: '/page13',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/reminders.html',
+        controller: 'remindersCtrl'
+      }
+    }
+  })
+
+  .state('menu.insights', {
+    url: '/page14',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/insights.html',
+        controller: 'insightsCtrl'
+      }
+    }
+  })
+
+  .state('menu.smartTools', {
+    url: '/page15',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/smartTools.html',
+        controller: 'smartToolsCtrl'
+      }
+    }
+  })
+
+  .state('menu.profile', {
+    url: '/page16',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/profile.html',
+        controller: 'profileCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page4')
+
+  
+
+});
